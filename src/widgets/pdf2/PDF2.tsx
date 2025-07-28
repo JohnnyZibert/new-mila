73d08e042ab24e3dfcdf6d912057bc1c71ebb602
eugeniu zgirdan <zgirdan.evg@gmail.com>
2025-07-28 15:08:49 +0300

    проба пинча

--- src/widgets/pdf2/PDF2.tsx
+++ src/widgets/pdf2/PDF2.tsx
@@ -39,7 +39,7 @@ export const PdfViewer2 = () => {
           overflow: "hidden",
           width: "100%",
           height: "100vh",
-          // WebkitOverflowScrolling: "touch",
+          WebkitOverflowScrolling: "touch",
         }}
       >
         <div
